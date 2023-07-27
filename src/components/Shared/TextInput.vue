3ac57a5c653abea3f745a493b8d65b63d6012941
Alper Gunindi <alpergunindi@hotmail.com>
2023-07-27 16:04:45 +0200

    Add V-Bind

---
+++ src/components/Shared/TextInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <input
+    type="text"
+    :value="value"
+    class="w-full text-lg font-normal focus:outline-none"
+    @input="handleInput"
+    />
+</template>
+
+<script>
+export default {
+    name: "TextInput",
+    data () {
+        return {
+            value: {
+                type: String,
+                required: true,
+            }
+        }
+    },
+    methods: {
+        handleInput ($event) {
+            this.$emit("handleInput", $event.target.value);
+        }
+    }
+}
+</script>
