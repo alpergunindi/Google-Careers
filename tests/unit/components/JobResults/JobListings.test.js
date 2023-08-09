fa193bbaa42d3da2238784e65aef0e58a53b38a5
Alper Gunindi <alpergunindi@hotmail.com>
2023-08-09 13:40:06 +0200

    Add test unit JobListings

--- tests/unit/components/JobResults/JobListings.test.js
+++ tests/unit/components/JobResults/JobListings.test.js
@@ -120,4 +120,26 @@ describe("JobListings", () => {
       expect(previousLink).toBeInTheDocument();
     });
   });
+
+  describe("when params exclude page number", () => {
+    it("displays page number 1", () => {
+      const queryParams = { page: undefined };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      expect(screen.getByText("Page 1")).toBeInTheDocument();
+    });
+  });
+
+  describe("when params include page number", () => {
+    it("displays page number", () => {
+      const queryParams = { page: "3" };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      expect(screen.getByText("Page 3")).toBeInTheDocument();
+    });
+  });
 });
